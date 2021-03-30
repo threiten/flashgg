1ffd03a0b8d561cebaf7fe8df6f43b2b09d7518b
Thomas Reitenspiess <thomas.reitenspiess@cern.ch>
2021-03-30 15:26:42 +0200

    Reapplying reverted changes IV

--- Taggers/plugins/SigmaMpTTagPreCleanerProducer.cc
+++ Taggers/plugins/SigmaMpTTagPreCleanerProducer.cc
@@ -155,6 +155,11 @@ namespace flashgg {
             std::map<std::string, float > extraWeightsMap;
             for (size_t itok=0; itok<compositeCandidatesTokens_.size(); itok++){
                 edm::Ptr<flashgg::WeightedCompositeCandidate> compcand = compositeCandidateHandles[itok]->ptrAt(candIndex);
+                if (compcand->hasWeight("JetBTagCutWeightCentral")){
+                    std::cout << "JetBTagCutWeightCentral for type " << compCandNames_[itok] << " SigmaMpTTagPreCleanerProducer: " << compcand->weight("JetBTagCutWeightCentral") << std::endl;
+                } else {
+                    std::cout << "JetBTagCutWeightCentral for type " << compCandNames_[itok] << " not found in SigmaMpTTagPreCleanerProducer !" << std::endl; 
+                }
                 compObjMap.insert(std::pair<std::string, edm::Ptr<flashgg::WeightedCompositeCandidate> >(compCandNames_[itok], compcand) );
                 for (vector<string>::const_iterator it = compcand->weightListBegin(); it != compcand->weightListEnd(); it++)
                     {
