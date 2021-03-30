d6580a3f8478847b347b602eb0ea1cacd6a408f6
Thomas Reitenspiess <thomas.reitenspiess@cern.ch>
2021-03-30 15:24:39 +0200

    Reapplying reverted changes III

--- DataFormats/src/WeightedCompositeCandidate.cc
+++ DataFormats/src/WeightedCompositeCandidate.cc
@@ -1,25 +1,3 @@
 #include "flashgg/DataFormats/interface/WeightedCompositeCandidate.h"
 
-namespace flashgg {
-  
-  template<class T>
-  void WeightedCompositeCandidate::addDaughter(const edm::Ptr<T> cand){
-    CompositeCandidate::addDaughter(*cand);
-    includeWeights(*cand);
-  }
-  
-  template<>
-  void WeightedCompositeCandidate::addDaughter(const edm::Ptr<flashgg::GenJetExtra> cand){
-    CompositeCandidate::addDaughter(*cand);
-  }
-  
-  template<>
-  void WeightedCompositeCandidate::addDaughter(const edm::Ptr<reco::GenJet> cand){
-    CompositeCandidate::addDaughter(*cand);
-  }
-  
-  template<>
-  void WeightedCompositeCandidate::addDaughter(const edm::Ptr<flashgg::GenLeptonExtra> cand){
-    CompositeCandidate::addDaughter(*cand);
-  }
-}
+// Implemented in the header
