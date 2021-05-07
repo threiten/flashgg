0c1d795c74034fcfdfa5b8e0a5193947958d8948
Thomas Reitenspiess <thomas.reitenspiess@cern.ch>
2021-05-07 17:17:11 +0200

    Removing some printouts

--- MicroAOD/interface/HadronicActivityProducer.h
+++ MicroAOD/interface/HadronicActivityProducer.h
@@ -100,14 +100,14 @@ namespace flashgg {
             
             int count = ( max_ > 0 ? max_ : collection.size() );
             bool isReco = false;
-            std::cout << "-------------------InputTags-----------------" << std::endl;
-            for ( auto &tag : srcTags) {
-                if (tag.label().find("reco") != std::string::npos || tag.label().find("Reco") != std::string::npos){
-                    isReco = true;
-                }
-                std::cout << "Source Tag: "<< tag << "isReco: " << isReco <<std::endl;
-            }
-            std::cout << "-------------------InputTags-----------------" << std::endl;
+            // std::cout << "-------------------InputTags-----------------" << std::endl;
+            // for ( auto &tag : srcTags) {
+            //     if (tag.label().find("reco") != std::string::npos || tag.label().find("Reco") != std::string::npos){
+            //         isReco = true;
+            //     }
+            //     std::cout << "Source Tag: "<< tag << "isReco: " << isReco <<std::endl;
+            // }
+            // std::cout << "-------------------InputTags-----------------" << std::endl;
             for( size_t iob = 0; iob<collection.size() && count > 0; ++iob ) {
                 auto & cand = collection.at(iob);
                 bool add = true;
@@ -116,18 +116,18 @@ namespace flashgg {
                 if( add ) {
                     out.addDaughter(cand);
                     const flashgg::WeightedObject *wObj = dynamic_cast<const flashgg::WeightedObject *>(&cand);
-                    if ( isReco ){
-                        if (wObj->hasWeight("JetBTagCutWeight")){
-                            std::cout << "JetBTagCutWeight HadronicActivityProducer: " << wObj->weight("JetBTagCutWeight") << std::endl;
-                        } else {
-                            std::cout << "JetBTagCutWeight HadronicActivityProducer not found!" << std::endl;
-                        }
-                        if (wObj->hasWeight("JetBTagCutWeightCentral")){
-                            std::cout << "JetBTagCutWeightCentral HadronicActivityProducer: " << wObj->weight("JetBTagCutWeightCentral") << std::endl;
-                        } else {
-                            std::cout << "JetBTagCutWeightCentral HadronicActivityProducer not found!" << std::endl;
-                        }
-                    }
+                    // if ( isReco ){
+                    //     if (wObj->hasWeight("JetBTagCutWeight")){
+                    //         std::cout << "JetBTagCutWeight HadronicActivityProducer: " << wObj->weight("JetBTagCutWeight") << std::endl;
+                    //     } else {
+                    //         std::cout << "JetBTagCutWeight HadronicActivityProducer not found!" << std::endl;
+                    //     }
+                    //     if (wObj->hasWeight("JetBTagCutWeightCentral")){
+                    //         std::cout << "JetBTagCutWeightCentral HadronicActivityProducer: " << wObj->weight("JetBTagCutWeightCentral") << std::endl;
+                    //     } else {
+                    //         std::cout << "JetBTagCutWeightCentral HadronicActivityProducer not found!" << std::endl;
+                    //     }
+                    // }
                     --count;
                 }
             }
